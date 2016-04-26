16eb184d8214f8c1acf3d0e020fd2746f49d88e6
jolan <jolan@jolan.org>
2016-04-26 17:13:53 -0500

    remove unneeded --prefix

--- build_windows.sh
+++ build_windows.sh
@@ -81,5 +81,5 @@ cp -f include/* .
 
 echo "Building cgminer."
 cd ..
-env CFLAGS="-DCURL_STATICLIB" CPPFLAGS="-I`pwd`/../cgminer-static/include -I`pwd`/../cgminer-static/mxe/usr/x86_64-w64-mingw32.static/include" LDFLAGS="-L`pwd`/../cgminer-static/lib -L`pwd`/../cgminer-static/mxe/usr/x86_64-w64-mingw32.static/lib" sh autogen.sh --enable-opencl --prefix=/home/jolan/mxe/usr/bin --host=x86_64-w64-mingw32 --build=x86_64-linux
+env CFLAGS="-DCURL_STATICLIB" CPPFLAGS="-I`pwd`/../cgminer-static/include -I`pwd`/../cgminer-static/mxe/usr/x86_64-w64-mingw32.static/include" LDFLAGS="-L`pwd`/../cgminer-static/lib -L`pwd`/../cgminer-static/mxe/usr/x86_64-w64-mingw32.static/lib" sh autogen.sh --enable-opencl --host=x86_64-w64-mingw32 --build=x86_64-linux
 make V=1
